4c5740b1613ed0244d6416f8f6765debba0f1496
makayla-star <54339097+makayla-star@users.noreply.github.com>
2019-10-08 12:39:52 -0500

    Add files via upload

---
+++ Deaprtments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDHW.Data
+{
+    public class Deaprtments
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+        public string DN { get; set; }
+    }
+}
---
+++ Employees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDHW.Data
+{
+    public class Employees
+    {
+     [Key]
+     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+     public int EID { get; set; }
+     public string FN { get; set; }
+     public string LN { get; set; }
+     [ForeignKey("DID")]
+     public Deaprtments DeptID { get; set; }
+    }
+
+}
